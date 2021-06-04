78fe2ad820ab3d6c0688fe634020e54636d5242e
Patricio Ruiz <patoruiz998@gmail.com>
2021-06-04 01:33:02 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+       [HttpPost] 
+       public ActionResult Index(string user, string password)
+        {
+            using(Models.MiSistemaEntities db = new Models.MiSistemaEntities())
+            {
+                var queryUser = (from u in db.usuario
+                                 where u.nombre == user && u.password == password
+                                 select u).FirstOrDefault();
+                if(queryUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(queryUser.nombre, true);
+                    return RedirectToAction("Index", "Admin");
+                    
+                    
+                }
+                else
+                {
+                    return View(); 
+
+                }
+
+                
+
+            }
+            
+        }
+    }
+}
